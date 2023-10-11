d7c233347b806181316e40c230e9ff8f9316f8da
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-10-11 19:59:54 +0300

    Added first commit

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+import Content from './components/Content/Content';
+import './App.css';
+// import { Button, Modal, Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className='App'>
+      <Content />
+    </div>
+  );
+}
+
+export default App;
